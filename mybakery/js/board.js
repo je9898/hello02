e10b2458debc10d85136f06b974744b2d04e6601
je9898 <younjeson@gmail.com>
2023-12-01 17:30:48 +0900

    231201-son

--- mybakery/js/board.js
+++ mybakery/js/board.js
@@ -1,211 +1,11 @@
 let setParam ={};
 let cnt = 1;
 let numberButton;
-let boardTable = [
-    {
-        boardId : 0,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20220109,
-        upDa : 20220120
-    },
-    {
-        boardId : 1,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20220109,
-        upDa : 20220120
-    },
-    {
-        boardId : 2,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20220129,
-        upDa : 20220110
-    },
-    {
-        boardId : 3,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20220128,
-        upDa : 20220111
-    },
-    {
-        boardId : 4,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20220120,
-        upDa : 20211225
-    },
-    {
-        boardId : 5,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20211220,
-        upDa : 20211225
-    },
-    {
-        boardId : 6,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20211220,
-        upDa : 20211225
-    },
-    {
-        boardId : 7,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20211220,
-        upDa : 20211225
-    },
-    {
-        boardId : 8,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20211220,
-        upDa : 20211225
-    },
-    {
-        boardId : 9,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20211220,
-        upDa : 20211225
-    },
-    {
-        boardId : 10,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20211220,
-        upDa : 20211225
-    },
-    {
-        boardId : 11,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20211220,
-        upDa : 20211225
-    },
-    {
-        boardId : 12,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20220109,
-        upDa : 20220120
-    },
-    {
-        boardId : 13,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20220129,
-        upDa : 20220110
-    },
-    {
-        boardId : 14,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20220128,
-        upDa : 20220111
-    },
-    {
-        boardId : 15,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20220120,
-        upDa : 20211225
-    },
-    {
-        boardId : 16,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20211220,
-        upDa : 20211225
-    },
-    {
-        boardId : 17,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20211220,
-        upDa : 20211225
-    },
-    {
-        boardId : 18,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20211220,
-        upDa : 20211225
-    },
-    {
-        boardId : 19,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20211220,
-        upDa : 20211225
-    },
-    {
-        boardId : 20,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20211220,
-        upDa : 20211225
-    },
-    {
-        boardId : 21,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20211220,
-        upDa : 20211225
-    },
-    {
-        boardId : 22,
-        userNo : 1,
-        title : "신제품 1+1",
-        con : "신제품 1+1 이벤트",
-        regDa : 20211220,
-        upDa : 20211225
-    }
 
-
-
-    
-
-]
-setParam.boardTable = boardTable;
+setParam = JSON.parse(localStorage.setParam || "{}");
 console.log(setParam);
 
-let bData= setParam.boardTable;
-/* 
-<tr>
-    <td>1</td>
-    <th>
-    <a href="#!">[공지사항] 개인정보 처리방침 변경안내처리방침</a>
-    <p>테스트</p>
-    </th>
-    <td>2017.07.13</td>
-</tr>
-*/
+let bData= setParam.boardTable || [];
 
 const COUNT_PER_PAGE = 5;
 const totalPageCount = () => {
@@ -213,11 +13,13 @@ const totalPageCount = () => {
    };
 
 let html = "";
-
+let tbody = document.querySelector("tbody");
 function setPageOf(count){
+    if(bData.length === 0){
+        return;
+    }
     cnt = count;
 
-    let tbody = document.querySelector("tbody");
     tbody.innerHTML = "";
     for(i=COUNT_PER_PAGE*(count-1); i<COUNT_PER_PAGE*count; i++){
         html = "<tr>" +
@@ -226,7 +28,7 @@ function setPageOf(count){
                 "    <a href='#! class='title_b' onclick = 'js:title_btn("+i+")'>"+ bData[i].title +"</a>" +
                 "    <p>테스트</p>" +
                 "    </th>" +
-                "    <td>"+ JSON.stringify(bData[i].upDa).substring(0,1) +"</td>" +
+                "    <td>"+ bData[i].upDa +"</td>" +
                 "</tr>" 
         tbody.innerHTML += html;
     }
@@ -243,6 +45,13 @@ const pageBtn = document.querySelector('.nuBtn');
 
 
 const setPageBtn = () => {
+
+    if(bData.length === 0){
+        let boardTable = document.querySelector(".board-table");
+        boardTable.innerHTML = "<h3 id='noBoard'>없다 !</h3>";
+        return;
+    }
+
     pageBtn.innerHTML = '';
     for(let i = 1; i <= totalPageCount(); i++){
     pageBtn.innerHTML +=  `<span class="number-button" onclick="setPageOf(${i})" > ${i} </span`;
